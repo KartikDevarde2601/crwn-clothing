47285af826ce13140d38e7f9c2de1403e42429f1
kartik satish devarde <kartikdevarde@outlook.com>
2023-08-10 13:48:07 +0530

    header change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
-
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
 
@@ -19,3 +19,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root') 
 );
+
+serviceWorker.register();
